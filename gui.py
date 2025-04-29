09647ad87bfc49c7d7fe243e4657c1e7f127697b
janhvi <janhviwork444@gmail.com>
2025-04-29 23:35:49 +0530

    all access

--- gui.py
+++ gui.py
@@ -1,1 +1,12 @@
-īṁōr̥ṭ
+import functions
+import FreeSimpleGUI as sg
+
+label= sg.Text("Type in a TO-DO")
+input_box = sg.InputText(tooltip = "Enter todo")
+add_button = sg.Button("Add")
+
+window = sg.Window('My To-Do App', layout=[[[label],[input_box, add_button]]])
+window.read()
+window.close()
+
+
