1531adf4429ea27a50cacc64f3120e2b0132bf39
janhvi <janhviwork444@gmail.com>
2025-04-30 18:09:50 +0530

    gui first development

--- functions.py
+++ functions.py
@@ -5,10 +5,11 @@ def get_todos(filepath= FILEPATH):
         todos_local = file_local.readlines()
     return todos_local
 
-def write_todos( todos_arg, FILEPATH):
+def write_todos( todos_arg, filepath = FILEPATH ):
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
 
 
 if __name__ == "__main__":
     print("Hello")
+
